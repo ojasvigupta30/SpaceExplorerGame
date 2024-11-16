b60bdedbcb0c26b327b8673f0a4cf7398e0dd101
ojasvigupta30 <ojasvigupta30@gmail.com>
2024-11-15 23:42:37 -0600

    Updated App.jsx file with the basic application structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import GameCanvas from './components/GameCanvas';
+import Heading from './components/Heading';
 
-function App() {
-  
-
+const App = () => {
   return (
-    <>
-      
-    </>
-  )
-}
+    <div>
+      <Heading />
+      <GameCanvas />
+    </div>
+  );
+};
 
-export default App
+export default App;
