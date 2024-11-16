234d2f7f0d38b62e7acb4620e4ef4fcc592a9031
ojasvigupta30 <ojasvigupta30@gmail.com>
2024-11-15 23:43:32 -0600

    Added the heading to the game application

---
+++ src/components/Heading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Heading = () => {
+  return (
+    <div className="heading">
+      <h1>Space Shooter Explorer</h1>
+    </div>
+  );
+};
+
+export default Heading;
